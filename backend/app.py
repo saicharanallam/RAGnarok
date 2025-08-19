de247d28b2918ff24ddfbb38a6c7f3638bd8138f
Sai Charan Allam <40341021+saicharanallam@users.noreply.github.com>
2025-08-19 22:47:08 +0530

    Update with swagger

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
+from flasgger import Swagger
 from models import db
 from routes import bp
 from config import Config
+from swagger_config import SWAGGER_CONFIG, SWAGGER_TEMPLATE
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+
+# Initialize Swagger
+swagger = Swagger(app, config=SWAGGER_CONFIG, template=SWAGGER_TEMPLATE)
+
 app.register_blueprint(bp)
 
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
